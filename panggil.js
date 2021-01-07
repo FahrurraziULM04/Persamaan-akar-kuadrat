d66e0bf3aab59052e0044ea3db9f97912ccacc04
lordsaint123 <74971083+lordsaint123@users.noreply.github.com>
2021-01-07 19:20:24 +0330

    Penghubung_Tahap 3
    Penghubung html tahap 3 dengan database

---
+++ panggil.js
@@ -0,0 +1,17 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyB2KhE2_xH9oaaurb6lmdVOI5tBMrPxrC4",
+    authDomain: "kuadratakar-fda8c.firebaseapp.com",
+    databaseURL: "https://kuadratakar-fda8c-default-rtdb.firebaseio.com",
+    projectId: "kuadratakar-fda8c",
+    storageBucket: "kuadratakar-fda8c.appspot.com",
+    messagingSenderId: "566656866298",
+    appId: "1:566656866298:web:d36604cd53103ed9ea7643",
+    measurementId: "G-85JXYRYY3J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+const database = firebase.database();
+
+ 
